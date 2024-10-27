f9d8b589a970bb22fde901cecabc153f4a1ff51b
Ted Osborne <eccentricon@users.noreply.github.com>
2024-10-27 11:34:04 -0400

    11. Data types

--- 01-Fundamentals-Part-1/starter_copy/script.js
+++ 01-Fundamentals-Part-1/starter_copy/script.js
@@ -1,14 +1,44 @@
-// let js = "amazing";
+function logDivider() {
+  const lineLength = 60;
+  const separator = "-";
+  console.log(separator.repeat(lineLength));
+}
+function logBanner(text) {
+  const lineLength = 60;
+  const separator = "-";
+
+  console.log(separator.repeat(lineLength));
+
+  // Calculate padding on each side
+  const padding = (lineLength - text.length) / 2;
+
+  // Use padStart and padEnd to center the text
+  console.log(" ".repeat(padding) + text + " ".repeat(padding));
+
+  console.log(separator.repeat(lineLength));
+}
+function logAssignment(text) {
+  // logDivider();
+  console.log("*** ASSIGNMENT", text, "***");
+}
+let lesson;
+
+// ---------------------------------------------------------------------------
+// 9. Variables and values
+// ---------------------------------------------------------------------------
+lesson = "9. Variables and values";
+logBanner(lesson);
+
+let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
-// console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
 
+logAssignment(lesson);
 /*
-Assignment: JavaScript Fundamentals — Part 1 / Values and Variables
-
-1.  Declare variables called country, continent and population and
-    assign their values according to your own country (population in millions).
-2.  Log their values to the console.
-*/
+ * 1.  Declare variables called country, continent and population and
+ *     assign their values according to your own country (population in millions).
+ * 2.  Log their values to the console.
+ */
 
 let country = "USA";
 let continent = "North America";
@@ -16,3 +46,44 @@ let population = 346; // population in millions
 console.log("country = ", country);
 console.log("continent = ", continent);
 console.log("population = ", population);
+
+// ---------------------------------------------------------------------------
+// 11. Data types
+// ---------------------------------------------------------------------------
+lesson = "11. Data types";
+logBanner(lesson);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); // a boolean value
+console.log(typeof javascriptIsFun); // a variable
+console.log(typeof 23); // a number
+console.log(typeof "Jonas"); // a string
+
+// dynamic typing
+javascriptIsFun = "YES!"; // now a string
+console.log(typeof javascriptIsFun); // a variable
+
+// undefined
+let year;
+console.log(typeof year); // undefined!
+year = 1961;
+console.log(typeof year);
+
+console.log(typeof null); // reports 'object' (legacy bug!)
+
+logAssignment(lesson);
+/*
+ * Declare a variable called isIsland and set its value according to your country.
+ * The variable should hold a Boolean value. Also declare a variable language,
+ * but don't assign it any value yet.
+ *
+ * Log the types of isIsland, population, country and language to the console.
+ */
+let isIsland = false;
+let language;
+
+console.log("isIsland:", typeof isIsland);
+console.log("population:", typeof population);
+console.log("country:", typeof country);
+console.log("language:", language);
--- notes.md
+++ notes.md
@@ -37,3 +37,60 @@ $ git pull origin master
   - Start variables with `lowercaseLetters`
   - `UPPERCASE` for constants
 - Can't use reserved keywords
+
+## 10. Practice assignments
+
+https://codingheroes.io/assignments/instructions.html
+
+## 11. Data types
+
+- 2 kinds of values:
+  - **Object**
+    ```
+    let me = {
+      name: 'Jonas'
+    }
+    ```
+  - **Primitive** (_everything else that isn't an object_)
+    ```
+    let firstName = 'Jonas';
+    let age = 30;
+    ```
+
+### 7 primitive data types
+
+First three are the biggies (**Number**, **String**, and **Boolean**).
+
+1. **Number** - Floating point numbers 👉 _Used for decimals and integers_\
+   `let age = 23;`
+
+2. **String** - Sequence of characters 👉 _Used for text; always quoted_\
+   `let firstName = 'Jonas';`
+
+3. **Boolean** - Logical type that can only be `true` or `false` 👉 _Used for making decisions_\
+   `let fullAge = false;`
+
+---
+
+4. **Undefined** - Value of variable that's not yet defined ("empty value")\
+   `let children;`
+
+5. **Null** - Also means "empty value"
+
+6. **Symbol** (ES2015) - Value that is unique and immutable 👉 _[Not useful for now]_
+
+7. _BigInt_ (ES2020 - Larger integers that the Number type cannot hold
+
+#### JavaScript has _dynamic typing_
+
+- We do not have to manually define the data type of a value stored in a variable
+- Data types are determined **automatically**
+- THe **value** has a type, _NOT_ the variable
+
+#### _typeof_ operator
+
+The [`typeof`] operator returns a string indicating the type of the operand's value.
+
+<!-- Reference links -->
+
+[`typeof`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
