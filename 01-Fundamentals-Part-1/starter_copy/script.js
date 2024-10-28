e8e3a365ddd2e491efa9cb970ea6dc2e83b39196
Ted Osborne <eccentricon@users.noreply.github.com>
2024-10-28 18:02:57 -0400

    JavaScript Fundamentals – Part 1: Coding challenge #1

--- 01-Fundamentals-Part-1/starter_copy/script.js
+++ 01-Fundamentals-Part-1/starter_copy/script.js
@@ -234,3 +234,58 @@ console.log(AGE_JONAS, AGE_SARAH, AVG_AGE); // 55.5 ? No!
 
 const AVERAGE_AGE = (AGE_JONAS + AGE_SARAH) / 2;
 console.log(AGE_JONAS, AGE_SARAH, AVERAGE_AGE); // 32.5 Yes!
+
+// ---------------------------------------------------------------------------
+// Coding Challenge 1
+// ---------------------------------------------------------------------------
+lesson = "Coding Challenge 1";
+logBanner(lesson);
+
+/*
+CHALLENGE #1
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula: BMI = mass / (height * height) (mass in kg and 
+height in meters).
+
+Your task is to write some code to help them:
+
+1. Store Mark's and John's mass and height in variables called massMark, 
+   heightMark, massJohn and heightJohn.
+
+2. Calculate both their BMIs using the formula, and store the results in two 
+   variables called BMIMark and BMIJohn.
+
+3. Log the value of BMIMark and BMIJohn to the console.
+
+4. BONUS: Create a boolean variable markHigherBMI containing information about 
+   whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is
+1.95 m tall.
+
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is
+1.76 m tall.
+
+ */
+
+// Test data 1:
+let massMark = 78; // mass in kg
+let heightMark = 1.69; // height in meters
+let massJohn = 92;
+let heightJohn = 1.95;
+
+// Test data 2:
+// let massMark = 95; // mass in kg
+// let heightMark = 1.88; // height in meters
+// let massJohn = 85;
+// let heightJohn = 1.76;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log("BMIMark:", BMIMark);
+console.log("BMIJohn:", BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log("markHigherBMI:", markHigherBMI);
