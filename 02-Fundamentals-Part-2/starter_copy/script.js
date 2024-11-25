c101d00be4b1af66762d8791f60a83a7cb18f50d
Ted Osborne <eccentricon@users.noreply.github.com>
2024-11-25 18:01:04 -0500

    40. Introduction to arrays (assignment)

--- 02-Fundamentals-Part-2/starter_copy/script.js
+++ 02-Fundamentals-Part-2/starter_copy/script.js
@@ -399,3 +399,57 @@ console.log(friends.includes("23")); // false, strict equality
 console.log(friends.includes(23)); // true
 
 if (friends.includes("Steven")) console.log("You have a friend named Steven.");
+
+logAssignment(lesson);
+/*
+ * 1. Create an array containing all the neighbouring countries of a country of your
+ *    choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+ *    into a variable called neighbours.
+ *
+ * 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+ *    your selected country, so add it to the end of the neighbours array.
+ *
+ * 3. Unfortunately, after some time the new country is dissolved, so remove it from the
+ *    end of the array.
+ *
+ * 4. If the neighbours array does not include the country 'Germany', log to the console:
+ *    'Probably not a central european country :D'.
+ *
+ * 5. Change the name of one of your neighbouring countries. To do that, find the index
+ *    of the country in the neighbours array, and then use that index to change the array
+ *    at that index position. For example, you can search for 'Sweden' in the array, and
+ *    then replace it with 'Republic of Sweden'.
+ */
+
+let neighbors = [
+  "North Korea",
+  "Russia",
+  "Mongolia",
+  "Kazakhstan",
+  "Kyrgyzstan",
+  "Tajikistan",
+  "Afghanistan",
+  "Pakistan",
+  "India",
+  "Nepal",
+  "Bhutan",
+  "Myanmar",
+  "Laos",
+  "Vietnam",
+];
+console.log(neighbors);
+console.log("Adding Utopia...");
+neighbors.push("Utopia");
+console.log(neighbors);
+console.log("Utopia dissolved...");
+neighbors.pop();
+console.log(neighbors);
+if (!neighbors.includes("Germany"))
+  console.log("Probably not a central european country :D");
+let indiaIndex = neighbors.indexOf("India");
+neighbors[indiaIndex] = "Republic of India";
+console.log(neighbors);
+
+// ---------------------------------------------------------------------------
+// lesson = logBanner("41. Coding challenge 2");
+// ---------------------------------------------------------------------------
