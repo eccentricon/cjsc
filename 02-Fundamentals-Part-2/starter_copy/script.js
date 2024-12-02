efad2016df9e3a8e11ae12a97da2ab44074c2682
Ted Osborne <eccentricon@users.noreply.github.com>
2024-12-02 17:45:35 -0500

    45. Coding challenge 3

--- 02-Fundamentals-Part-2/starter_copy/script.js
+++ 02-Fundamentals-Part-2/starter_copy/script.js
@@ -661,3 +661,61 @@ myCountry.checkIsland = function () {
 console.log(
   `${myCountry.country} ${myCountry.checkIsland() ? "is" : "is not"} an island.`
 );
+
+// ---------------------------------------------------------------------------
+lesson = logBanner("45. Coding challenge 3");
+// ---------------------------------------------------------------------------
+
+/*
+ * Let's go back to Mark and John comparing their BMIs!
+ *
+ * This time, let's use objects to implement the calculations!
+ * Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+ *
+ * Your tasks:
+ *
+ * 1. For each of them, create an object with properties for their full name,
+ *    mass, and height (Mark Miller and John Smith). Name these objects as mark
+ *    and john, and their properties exactly as fullName, mass and height.
+ *
+ * 2. Create a calcBMI method on each object to calculate the BMI (the same method
+ *    on both objects). Assign the BMI value to a property called bmi (lowercase),
+ *    and also return it from the method.
+ *
+ * 3. Log to the console who has the higher BMI, together with the full name and the
+ *    respective BMI. Example:
+ *    "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+ *
+ * TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+ *
+ * IMPORTANT: The ** operator is not supported in this editor. Please make sure to use
+ * exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+ */
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78, // kg
+  height: 1.69, // meters
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92, // kg
+  height: 1.95, // meters
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+let higher = mark;
+let lower = john;
+if (john.calcBMI() > mark.calcBMI()) {
+  higher = john;
+  lower = mark;
+}
+console.log(
+  `${higher.fullName}'s BMI (${higher.bmi}) is ` +
+    `higher than ${lower.fullName}'s (${lower.bmi})!`
+);
